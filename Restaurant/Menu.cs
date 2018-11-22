f4c8d7f8d8363a63508771e7ae43de79c0bb100f
mystified131 <mystifiedthomas@gmail.com>
2018-11-22 10:02:49 -0600

    Initial commit.

---
+++ Restaurant/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    class Menu
+    {
+        public string Name { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+        public Menu(string name, DateTime lastUpdated)
+
+        {
+            name = Name;
+            lastUpdated = LastUpdated;
+      
+        }
+
+        public List <Dictionary<string, double>> Menuitems { get; set; }
+
+
+        public override String ToString()
+        {
+            return Name + " (Name: " + Name + ", Last Updated: " + LastUpdated + ")";
+        }
+
+        public override bool Equals(Object o)
+        {
+            if (o == this)
+            {
+                return true;
+            }
+
+            if ((o == null) & o.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Menu menuObj = o as Menu;
+            return Name == menuObj.Name;
+        }
+
+
+    }
+}
+
+
---
+++ Restaurant/MenuItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    class MenuItem
+    {
+        public string Name { get; set; }
+        public Double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+
+        public MenuItem(string name, Double price)
+
+        { name = Name;
+            price = Price;
+        }
+
+        public override String ToString()
+        {
+            return Name + " (Item Name: " + Name + ", Price " + Price + ")";
+        }
+
+        public override bool Equals(Object o)
+        {
+            if (o == this)
+            {
+                return true;
+            }
+
+            if ((o == null) & o.GetType() != GetType())
+            {
+                return false;
+            }
+
+            Student menuitemObj = o as Student;
+            return Name == menuitemObj.Name;
+        }
+
+
+    }
+}
